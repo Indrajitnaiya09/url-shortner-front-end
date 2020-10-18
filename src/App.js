9286ec9642d7363513f41522fc09f23a95fe67e3
Indrajit Naiya <indrajitnaiya09@gmail.com>
2020-10-18 20:14:26 +0530

    Added: -- Dependent package added Implemented: -- Backend api call Implemented -- Display message from response implemented -- swal implemented

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import React from 'react';
+import Axios from 'axios';
+import Swal from 'sweetalert';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import swal from 'sweetalert';
+import _ from 'lodash';
+
+
 
 function App() {
+  const [data, setData] = React.useState("")
+  const [shortUrl, setShortUrl] = React.useState("")
+  const handleChange = (e) => {
+    setData(e.target.value)
+ }
+  const urlShortner = () => {
+    Axios.post('https://ed2d22541646.ngrok.io/urls', {
+      "url":{
+          "original_url":data
+      }
+  })
+    .then((response) =>{
+      setShortUrl(response.data.url.short_url)
+      console.log(response.data.url.short_url)
+    })
+    .catch((err) =>{
+      Swal("Please enter correct url", "", "error")
+    })
+  }
+  console.log(data);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1> Welcome to my shortner project. </h1>
+      <input type="text" placeholder="URL" value={data} onChange={handleChange} ></input>
+      <button type="button" onClick={urlShortner} >Click Me!</button>
+      <br/> <br/>
+      {!_.isEmpty(shortUrl) && (<p>Your short url {`https://ed2d22541646.ngrok.io/${shortUrl}`}</p>)}
     </div>
   );
 }
